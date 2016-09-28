ac821e82dc7a1de163d9727e92903be3430342bf
Sami Niemi <Sami.Niemi@valtech.co.uk>
2016-09-28 10:57:04 +0100

    Example how to use blocking and different distance metrics.

--- AddressIndex/Analytics/fuzzymatchingExample.py
+++ AddressIndex/Analytics/fuzzymatchingExample.py
@@ -4,7 +4,7 @@ ONS Address Index - Fuzzy Matching Test
 
 A simple example demonstrating fuzzy matching.
 
-Uses Levenshtein Distance, see here:
+Uses e.g. Levenshtein Distance, see here:
 https://en.wikipedia.org/wiki/Levenshtein_distance
 See also:
 http://chairnerd.seatgeek.com/fuzzywuzzy-fuzzy-string-matching-in-python/
@@ -28,8 +28,8 @@ Author
 Version
 -------
 
-:version: 0.1
-:date: 27-Sep-2016
+:version: 0.2
+:date: 28-Sep-2016
 """
 from AddressIndex.Analytics import data
 import pandas as pd
@@ -37,6 +37,7 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import recordlinkage
 import datetime
+import pprint
 
 
 def getData():
@@ -50,6 +51,21 @@ def getData():
     print('\nFound', len(df.index), 'addresses...')
 
     return df
+
+
+def hammingDistance(s1, s2):
+    """
+    Return the Hamming distance between equal-length sequences
+
+    :param s1:
+    :param s2:
+
+    :return:
+    """
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+
+    return sum(el1 != el2 for el1, el2 in zip(s1, s2))
 
 
 def runSimpleTest1(find_address='6 PROSPECT GARDENS EXTER EX4 6BA'):
@@ -75,7 +91,7 @@ def runSimpleTest1(find_address='6 PROSPECT GARDENS EXTER EX4 6BA'):
     print('First is correct?', test_against_original == matches[0][0])
 
     stop = datetime.datetime.now()
-    print('\nRun in', (stop - start).seconds, 'seconds...')
+    print('\nRun in', round((stop - start).microseconds/1.e6, 2), 'seconds...')
 
 
 def runSimpleTest2(find_address={'street': ['6 PROSPCT GARDNS EXTER',], 'postcode': ['EX4 6TA',]}):
@@ -84,6 +100,9 @@ def runSimpleTest2(find_address={'street': ['6 PROSPCT GARDNS EXTER',], 'postcod
     :param find_address:
     :return:
     """
+    print('Matching')
+    pprint.pprint(find_address)
+
     # get data from the database against which we are linking
     df = getData()
     df['postcode'] = df.apply(data._getPostcode, axis=1)
@@ -99,20 +118,31 @@ def runSimpleTest2(find_address={'street': ['6 PROSPCT GARDNS EXTER',], 'postcod
     # set blocking
     pcl = recordlinkage.Pairs(df, find)
     pairs = pcl.block('postcode')
-    print('After blocking, need to test', len(pairs))
+    print('\nAfter blocking, need to test', len(pairs))
 
-    # compare the two data sets - use different metrics
+    # compare the two data sets - use different metrics for the comparison
     compare = recordlinkage.Compare(pairs, df, find)
-    compare.string('postcode', 'postcode', method='jarowinkler', threshold=0.95)
-    compare.string('street', 'street', method='damerau_levenshtein', threshold=0.8)
+    # compare.string('postcode', 'postcode', method='jarowinkler', threshold=0.95, name='postcode')
+    # compare.string('street', 'street', method='damerau_levenshtein', threshold=0.85, name='street')
+    compare.string('postcode', 'postcode', method='jarowinkler', name='postcode_jw')
+    compare.string('street', 'street', method='damerau_levenshtein', name='street_dl')
 
     # The comparison vectors
+    print('\nComparsing vectors:')
     print(compare.vectors)
 
+    # find the matches and the best match
+    matchmetrics = compare.vectors.sum(axis=1)
+    potentialMatches = matchmetrics.index.levels[0].tolist()
+    print('\nPotential Matches:')
+    print(df.loc[potentialMatches])
+    print('\nBest Match:')
+    print(df.loc[matchmetrics.argmax()[0]])
+
     stop = datetime.datetime.now()
-    print('\nRun in', (stop - start).seconds, 'seconds...')
+    print('\nRun in', round((stop - start).microseconds/1.e6, 2), 'seconds...')
 
 
 if __name__ == "__main__":
-    # runSimpleTest1()
+    runSimpleTest1()
     runSimpleTest2()
