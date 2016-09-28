495c72f9f70d16c3445dc39938229545a2c45d00
Sami Niemi <Sami.Niemi@valtech.co.uk>
2016-09-28 17:14:39 +0100

    Updated the fuzzy matching example.

--- AddressIndex/Analytics/fuzzymatchingExample.py
+++ AddressIndex/Analytics/fuzzymatchingExample.py
@@ -42,10 +42,10 @@ import pprint
 
 def getData():
     """
-    Get data from the PostGres database
+    Get data from the PostGre database.
 
     :return: dataframe containing the queried data
-    :rtype: pandas dataframe
+    :rtype: pandas.DataFrame
     """
     df = data.queryDB('SELECT address FROM addresses')
     print('\nFound', len(df.index), 'addresses...')
@@ -55,12 +55,13 @@ def getData():
 
 def hammingDistance(s1, s2):
     """
-    Return the Hamming distance between equal-length sequences
+    Return the Hamming distance between equal-length sequences.
 
-    :param s1:
-    :param s2:
+    :param s1: test string
+    :param s2: test string
 
-    :return:
+    :return: Hamming distance
+    :rtype: int
     """
     if len(s1) != len(s2):
         raise ValueError("Undefined for sequences of unequal length")
@@ -68,9 +69,16 @@ def hammingDistance(s1, s2):
     return sum(el1 != el2 for el1, el2 in zip(s1, s2))
 
 
-def runSimpleTest1(find_address='6 PROSPECT GARDENS EXTER EX4 6BA'):
+def runSimpleTest1(find_address='6 PROSPECT GARDENS EXTER EX4 6BA', limit=3):
     """
-    simple test using Levenshtein Distance. Naive solution where the input is tested against all possibilities.
+    Simple test using String similarity. Compares the input address against the first entry of the database.
+
+    Naive solution to find the likeliest matches. The input is tested against all possibilities i.e. no blocking.
+
+    :param find_address: a string defining the address that is being matched
+    :type find_address: str
+    :param limit: maximum number of matches that are returned
+    :type limit: int
 
     :return: None
     """
@@ -78,7 +86,7 @@ def runSimpleTest1(find_address='6 PROSPECT GARDENS EXTER EX4 6BA'):
 
     test_against_original = df['address'][0]
 
-    print('\nSimple Test:')
+    print('\nSimple String Similarity Test:')
     print(test_against_original, 'vs')
     print(find_address)
     print('Ratio:', fuzz.ratio(test_against_original, find_address))
@@ -87,7 +95,7 @@ def runSimpleTest1(find_address='6 PROSPECT GARDENS EXTER EX4 6BA'):
     print('\nFinding three best matches for', find_address, '...')
     start = datetime.datetime.now()
 
-    matches = process.extract(find_address, df['address'], limit=3)
+    matches = process.extract(find_address, df['address'], limit=limit)
 
     print(matches)
     print('First is correct?', test_against_original == matches[0][0])
@@ -98,9 +106,14 @@ def runSimpleTest1(find_address='6 PROSPECT GARDENS EXTER EX4 6BA'):
 
 def runSimpleTest2(find_address={'street': ['6 PROSPCT GARDNS EXTER',], 'postcode': ['EX4 6TA',]}):
     """
+    Matching a given address against the database.
+    Uses postcode blocking and Damerau - Levenshtein distance for the remaining address.
+    Finds the best match by identifying the maximum of the sum of the comparison vectors.
 
     :param find_address:
-    :return:
+    :type find_address: dict
+
+    :return: None
     """
     print('Matching')
     pprint.pprint(find_address)
@@ -123,14 +136,15 @@ def runSimpleTest2(find_address={'street': ['6 PROSPCT GARDNS EXTER',], 'postcod
     print('\nAfter blocking, need to test', len(pairs))
 
     # compare the two data sets - use different metrics for the comparison
-    compare = recordlinkage.Compare(pairs, df, find)
+    compare = recordlinkage.Compare(pairs, df, find, batch=True)
     # compare.string('postcode', 'postcode', method='jarowinkler', threshold=0.95, name='postcode')
     # compare.string('street', 'street', method='damerau_levenshtein', threshold=0.85, name='street')
     compare.string('postcode', 'postcode', method='jarowinkler', name='postcode_jw')
     compare.string('street', 'street', method='damerau_levenshtein', name='street_dl')
+    compare.run()
 
     # The comparison vectors
-    print('\nComparsing vectors:')
+    print('\nComparison vectors:')
     print(compare.vectors)
 
     # find the matches and the best match
