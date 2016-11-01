6bda10e7a7c37cd31c05fec1d00f650810b90d82
Aliaksandr.Bedrytski <aliaksandr.bedrytski@valtech.co.uk>
2016-11-01 16:07:57 +0000

    Changes properties' names for the stored documents in Elastic

--- batch/src/main/scala/uk/gov/ons/addressindex/models/CSVSchemas.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/models/CSVSchemas.scala
@@ -2,18 +2,46 @@ package uk.gov.ons.addressindex.models
 
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
+/**
+  * Contains schemas that should be applied to CSV documents
+  */
 object CSVSchemas {
 
-  private val deliveryPointFields = List("RECORD_IDENTIFIER","CHANGE_TYPE","PRO_ORDER","UPRN","UDPRN",
-    "ORGANISATION_NAME","DEPARTMENT_NAME","SUB_BUILDING_NAME","BUILDING_NAME","BUILDING_NUMBER",
-    "DEPENDENT_THOROUGHFARE","THROUGHFARE","DOUBLE_DEPENDENT_LOCALITY","DEPENDENT_LOCALITY",
-    "POST_TOWN","POSTCODE","POSTCODE_TYPE","DELIVERY_POINT_SUFFIX","WELSH_DEPENDENT_THOROUGHFARE",
-    "WELSH_THOROUGHFARE","WELSH_DOUBLE_DEPENDENT_LOCALITY","WELSH_DEPENDENT_LOCALITY","WELSH_POST_TOWN",
-    "PO_BOX_NUMBER","PROCESS_DATE","START_DATE","END_DATE","LAST_UPDATE_DATE","ENTRY_DATE")
-
-  val deliveryPointSchema = StructType(
-    deliveryPointFields.map(fieldName => StructField(fieldName, StringType, nullable = false))
-  )
+  /**
+    * Delivery point CSV file schema
+    */
+  val deliveryPointSchema = StructType(Seq(
+    StructField("recordIdentifier", StringType, nullable = false),
+    StructField("changeType", StringType, nullable = false),
+    StructField("proOrder", StringType, nullable = false),
+    StructField("uprn", StringType, nullable = false),
+    StructField("udprn", StringType, nullable = false),
+    StructField("organizationName", StringType, nullable = false),
+    StructField("departmentName", StringType, nullable = false),
+    StructField("subBuildingName", StringType, nullable = false),
+    StructField("buildingName", StringType, nullable = false),
+    StructField("buildingNumber", StringType, nullable = false),
+    StructField("dependentThoroughfare", StringType, nullable = false),
+    StructField("thoroughfare", StringType, nullable = false),
+    StructField("doubleDependentLocality", StringType, nullable = false),
+    StructField("dependentLocality", StringType, nullable = false),
+    StructField("postTown", StringType, nullable = false),
+    StructField("postcode", StringType, nullable = false),
+    StructField("postcodeType", StringType, nullable = false),
+    StructField("deliveryPointSuffix", StringType, nullable = false),
+    StructField("welshDependentThoroughfare", StringType, nullable = false),
+    StructField("welshThoroughfare", StringType, nullable = false),
+    StructField("welshDoubleDependentLocality", StringType, nullable = false),
+    StructField("welshDependentLocality", StringType, nullable = false),
+    StructField("welshDependentLocality", StringType, nullable = false),
+    StructField("welshPostTown", StringType, nullable = false),
+    StructField("poBoxNumber", StringType, nullable = false),
+    StructField("processDate", StringType, nullable = false),
+    StructField("startDate", StringType, nullable = false),
+    StructField("endDate", StringType, nullable = false),
+    StructField("lastUpdateDate", StringType, nullable = false),
+    StructField("entryDate", StringType, nullable = false)
+  ))
 
 
 }
