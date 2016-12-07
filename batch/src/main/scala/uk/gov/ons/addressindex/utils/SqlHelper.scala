46e987e784cde2ca943dc1716c3f74fac534487d
nesstest <steven.thorne@ons.gov.uk>
2016-12-07 15:47:08 +0000

    Implements joins for addresses that have two language descriptions.

--- batch/src/main/scala/uk/gov/ons/addressindex/utils/SqlHelper.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/utils/SqlHelper.scala
@@ -7,21 +7,50 @@ import org.apache.spark.sql.DataFrame
   */
 object SqlHelper {
 
-  def joinCsvs(blpu: DataFrame, lpi: DataFrame, organisation: DataFrame, street: DataFrame,
+  def joinCsvs(blpu: DataFrame, lpi: DataFrame, organisation: DataFrame, classification: DataFrame, street: DataFrame,
                streetDescriptor: DataFrame): DataFrame = {
     blpu.registerTempTable("blpu")
     organisation.registerTempTable("organisation")
+    classification.registerTempTable("classification")
     lpi.registerTempTable("lpi")
     street.registerTempTable("street")
     streetDescriptor.registerTempTable("streetDesc")
 
     SparkProvider.sqlContext.sql(
-      """SELECT blpu.uprn, org.organisation, lpi.paoText, lpi.paoStartNumber, lpi.saoText, st.streetDescriptor,
-        |st.townName, st.locality
-        |FROM blpu
-        |JOIN organisation org ON blpu.uprn = org.uprn
-        |JOIN lpi ON blpu.uprn = lpi.uprn
-        |JOIN street on lpi.usrn = street.usrn
-        |JOIN streetDesc st on street.usrn = st.usrn""".stripMargin)
+      """SELECT
+          blpu.uprn,
+          blpu.postcodeLocator,
+          blpu.addressbasePostal,
+          blpu.latitude,
+          blpu.longitude,
+          blpu.xCoordinate as easting,
+          blpu.yCoordinate as northing,
+          org.organisation,
+          org.legalName,
+          c.classificationCode,
+          lpi.usrn,
+          lpi.lpiKey,
+          lpi.paoText,
+          lpi.paoStartNumber,
+          lpi.paoStartSuffix,
+          lpi.paoEndNumber,
+          lpi.paoEndSuffix,
+          lpi.saoText,
+          lpi.saoStartNumber,
+          lpi.saoStartSuffix,
+          lpi.saoEndNumber,
+          lpi.saoEndSuffix,
+          lpi.level,
+          lpi.officialFlag,
+          lpi.logicalStatus,
+          st.streetDescriptor,
+          st.townName,
+          st.locality
+        FROM blpu
+        LEFT JOIN organisation org ON blpu.uprn = org.uprn
+        LEFT JOIN classification c ON blpu.uprn = c.uprn
+        LEFT JOIN lpi ON blpu.uprn = lpi.uprn
+        LEFT JOIN street ON lpi.usrn = street.usrn
+        LEFT JOIN streetDesc st ON street.usrn = st.usrn AND lpi.language = st.language""").na.fill("")
   }
 }
--- batch/src/test/scala/uk/gov/ons/addressindex/utils/SqlHelperSpec.scala
+++ batch/src/test/scala/uk/gov/ons/addressindex/utils/SqlHelperSpec.scala
@@ -1,34 +1,77 @@
 package uk.gov.ons.addressindex.utils
 
-import com.sun.xml.internal.ws.resources.AddressingMessages
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 import uk.gov.ons.addressindex.readers.AddressIndexFileReader
 
 /**
   * Test that the csv files are joined correctly.
   */
-class SqlHelperSpec extends FlatSpec with Matchers {
+class SqlHelperSpec extends WordSpec with Matchers {
 
-  "SqlHelper" should
+  "SqlHelper" should {
     "join blpu, organisation, lpi, street and street_descriptor" in {
 
-    // When
-    val result = SqlHelper.joinCsvs(AddressIndexFileReader.readBlpuCSV(), AddressIndexFileReader.readLpiCSV(),
-      AddressIndexFileReader.readOrganisationCSV(), AddressIndexFileReader.readStreetCSV(),
-      AddressIndexFileReader.readStreetDescriptorCSV()).collect()
+      // When
+      val result = SqlHelper.joinCsvs(AddressIndexFileReader.readBlpuCSV(), AddressIndexFileReader.readLpiCSV(),
+        AddressIndexFileReader.readOrganisationCSV(), AddressIndexFileReader.readClassificationCSV(),
+        AddressIndexFileReader.readStreetCSV(), AddressIndexFileReader.readStreetDescriptorCSV()).collect()
 
-    // Then
-    result.length shouldBe 1
+      // Then
+      result.length shouldBe 4
 
-    val firstLine = result(0)
+      val firstLine = result(1)
 
-    firstLine.getString(0) shouldBe "100010971564" // UPRN
-    firstLine.getString(1) shouldBe "SOME COUNCIL" // ORGANISATION
-    firstLine.getString(2) shouldBe "ANOTHER BUILDING NAME OR DESCRIPTION" // PAO_TEXT
-    firstLine.getString(3) shouldBe "15" // PAO_START_NUMBER
-    firstLine.getString(4) shouldBe "A BUILDING NAME OR DESCRIPTION" // SAO_TEXT
-    firstLine.getString(5) shouldBe "A STREET DESCRIPTOR" // STREET_DESCRIPTOR
-    firstLine.getString(6) shouldBe "TOWNY TOWN" // TOWN_NAME
-    firstLine.getString(7) shouldBe "A GREAT LOCALITY" // LOCALITY
+      firstLine.getString(0) shouldBe "100010971564" // UPRN
+      firstLine.getString(1) shouldBe "KL8 1JQ" // POSTCODE_LOCATOR
+      firstLine.getString(2) shouldBe "D" // ADDRESSBASE_POSTAL
+      firstLine.getString(3) shouldBe "53.6111710" // LATITUDE
+      firstLine.getString(4) shouldBe "-2.3158117" // LONGITUDE
+      firstLine.getString(5) shouldBe "379203.00" // X_COORDINATE
+      firstLine.getString(6) shouldBe "412780.00" // Y_COORDINATE
+      firstLine.getString(7) shouldBe "SOME COUNCIL" // ORGANISATION
+      firstLine.getString(8) shouldBe "THE LEGAL NAME" // LEGAL_NAME
+      firstLine.getString(9) shouldBe "RD" // CLASSIFICATION_CODE
+      firstLine.getString(10) shouldBe "9401385" // USRN
+      firstLine.getString(11) shouldBe "1610L000015314" // LPI_KEY
+      firstLine.getString(12) shouldBe "ANOTHER BUILDING NAME OR DESCRIPTION" // PAO_TEXT
+      firstLine.getString(13) shouldBe "15" // PAO_START_NUMBER
+      firstLine.getString(14) shouldBe "CC" // PAO_START_SUFFIX
+      firstLine.getString(15) shouldBe "9876" // PAO_END_NUMBER
+      firstLine.getString(16) shouldBe "AB" // PAO_END_SUFFIX
+      firstLine.getString(17) shouldBe "A BUILDING NAME OR DESCRIPTION" // SAO_TEXT
+      firstLine.getString(18) shouldBe "1234" // SAO_START_NUMBER
+      firstLine.getString(19) shouldBe "AA" // SAO_START_SUFFIX
+      firstLine.getString(20) shouldBe "5678" // SAO_END_NUMBER
+      firstLine.getString(21) shouldBe "BB" // SAO_END_SUFFIX
+      firstLine.getString(22) shouldBe "VERTICAL POSITION" // LEVEL
+      firstLine.getString(23) shouldBe "Y" // OFFICIAL_FLAG
+      firstLine.getString(24) shouldBe "1" // LOGICAL_STATUS
+      firstLine.getString(25) shouldBe "A STREET DESCRIPTOR" // STREET_DESCRIPTOR
+      firstLine.getString(26) shouldBe "TOWNY TOWN" // TOWN_NAME
+      firstLine.getString(27) shouldBe "A GREAT LOCALITY" // LOCALITY
+    }
+
+    "join blpu, organisation, lpi, street and street_descriptor for English and Welsh address" in {
+
+      // When
+      val result = SqlHelper.joinCsvs(AddressIndexFileReader.readBlpuCSV(), AddressIndexFileReader.readLpiCSV(),
+        AddressIndexFileReader.readOrganisationCSV(), AddressIndexFileReader.readClassificationCSV(),
+        AddressIndexFileReader.readStreetCSV(), AddressIndexFileReader.readStreetDescriptorCSV()).collect()
+
+      // Then
+      result.length shouldBe 4
+
+      val firstLine = result(0)
+
+      firstLine.getString(0) shouldBe "100010971565" // UPRN
+      firstLine.getString(10) shouldBe "9402538" // USRN
+      firstLine.getString(27) shouldBe "FSDF DSFSDF DSF" // LOCALITY
+
+      val secondLine = result(3)
+
+      secondLine.getString(0) shouldBe "100010971565" // UPRN
+      secondLine.getString(10) shouldBe "9402538" // USRN
+      secondLine.getString(27) shouldBe "LOCALITY XYZ" // LOCALITY
+    }
   }
 }
