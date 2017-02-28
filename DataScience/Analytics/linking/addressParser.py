80ab8f598a021e656ed85c9949bfe2257ecb5714
Sami Niemi <Sami.Niemi@valtech.co.uk>
2017-02-28 11:55:48 +0000

    A new class that can be used to do normalisation, probabilistic parsing, and post-processing.

--- DataScience/Analytics/linking/addressParser.py
+++ DataScience/Analytics/linking/addressParser.py
@@ -1,14 +1,15 @@
 """
 ONS Address Index - Complete Address Parser
-==================================
+===========================================
 
-A simple script to check the logic for extracting SAO information from an input string.
+This file contains an Address Parser class that can perform normalisation, probabilistic parsing, and post-processing.
 
 
 Requirements
 ------------
 
-:requires: pandas
+:requires: pandas (tested with 0.19.2)
+:requires: numpy (tested with 1.12.0)
 :requires: ProbabilisticParser (a CRF model specifically build for ONS)
 :requires: tqdm (4.10.0: https://github.com/tqdm/tqdm)
 
@@ -25,6 +26,12 @@ Version
 :version: 0.1
 :date: 28-Feb-2017
 """
+import logging
+import os
+import re
+import sys
+
+import numpy as np
 import pandas as pd
 from ProbabilisticParser import parser
 from tqdm import tqdm
@@ -35,8 +42,80 @@ class AddressParser:
     Address Parser class that implements the probabilistic parser and required pre- and post-processing steps.
     """
 
+    def __init__(self, log=None, **kwargs):
+        if log is None:
+            log = logging.getLogger()
+            log.addHandler(logging.StreamHandler(sys.stdout).setLevel(logging.DEBUG))
+
+        self.log = log
+
+        # relative path when referring to data files
+        self.currentDirectory = os.path.dirname(__file__)  # for relative path definitions
+        self.settings = dict(expandSynonyms=True)
+        self.settings.update(kwargs)
+
     @staticmethod
-    def _normalize_input_data(data, normalised_field_name='ADDRESS_norm'):
+    def _extract_postcode(string):
+        """
+        A static private method to extract a postcode from address string.
+
+        Uses a rather loose regular expression, so  may get some strings that are not completely valid postcodes.
+        Should not be used to validate whether a postcode conforms to the UK postcode standards.
+
+        The regular expression was taken from:
+        http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
+
+        :param string: string to be parsed
+        :type string: str
+
+        :return: postcode
+        :rtype: str
+        """
+        regx = r'(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))'
+        try:
+            potential_postcode = re.findall(regx, string)[0][0]
+            potential_postcode = potential_postcode.lower().strip()
+        except IndexError:
+            potential_postcode = None
+
+        # above regex gives also those without space between, add if needed
+        if potential_postcode is not None:
+            if ' ' not in potential_postcode:
+                inc = potential_postcode[-3:]
+                out = potential_postcode.replace(inc, '')
+                potential_postcode = out + ' ' + inc
+
+        return potential_postcode
+
+    @staticmethod
+    def _fix_london_boroughs(parsed, directory, datafile='localities.csv'):
+        """
+        A static private method to address incorrectly parsed London boroughs.
+
+        If the street name contains London borough then move it to locality and remove from the street name.
+
+        :param parsed: a dictionary containing the address tokens that have been parsed
+        :type parsed: dict
+        :param directory: location of the data file
+        :type directory: str
+        :param datafile: name of the data file containing a column locality
+        :type datafile: str
+
+        :return: a dictionary containing the address tokens with updated information
+        :rtype: dict
+        """
+        london_localities = pd.read_csv(directory + datafile)['locality']
+
+        for LondonLocality in london_localities:
+            if parsed['StreetName'].strip().endswith(LondonLocality):
+                parsed['Locality'] = LondonLocality
+                # take the last part out, so that e.g. CHINGFORD AVENUE CHINGFORD is correctly processed
+                # need to be careful with e.g.  WESTERN GATEWAY ROYAL VICTORIA DOCK (3 parts to remove)
+                parsed['StreetName'] = parsed['StreetName'].strip()[:-len(LondonLocality)].strip()
+
+        return parsed
+
+    def _normalize_input_data(self, data, normalised_field_name='ADDRESS_norm'):
         """
         Normalise input address information.
 
@@ -82,10 +161,33 @@ class AddressParser:
         data[normalised_field_name] = \
             data[normalised_field_name].str.replace(r'(\d+[a-z])(\s*-\s*)(\d+[a-z])', r'\1-\3', case=False)
 
+        # synonyms to expand - read from a file with format (from, to)
+        synonyms = pd.read_csv(os.path.join(self.currentDirectory, '../../data/') + 'synonyms.csv').values
+
+        # expand common synonyms to help with parsing
+        if self.settings['expandSynonyms']:
+            self.log.info('Expanding synonyms as a part of normalisation...')
+            for fro, to in synonyms:
+                data['ADDRESS_norm'] = data['ADDRESS_norm'].str.replace(fro, to)
+
+        # parsing gets really confused if region or county is in the line - get known counties from a file
+        counties = pd.read_csv(os.path.join(self.currentDirectory, '../../data/') + 'counties.csv')['county']
+
+        # use this for the counties so that e.g. ESSEX ROAD does not become just ROAD...
+        # todo: the regex is getting ridiculous, maybe do other way around i.e. country must be followed by postcode or
+        #       be the last component.
+        addRegex = r'(?:\s|$)(?!ROAD|LANE|STREET|CLOSE|DRIVE|AVENUE|SQUARE|COURT|PARK|CRESCENT|WAY|WALK|HEOL|FFORDD|HILL|GARDENS|GATE|GROVE|HOUSE|VIEW|BUILDING|VILLAS|LODGE|PLACE|ROW|WHARF|RISE|TERRACE|CROSS|ENTERPRISE|HATCH|&)'
+
+        # remove county from address but add a column for it
+        data['County'] = None
+        for county in counties:
+            msk = data[normalised_field_name].str.contains(county + addRegex, regex=True, na=False)
+            data.loc[msk, 'County'] = county
+            data[normalised_field_name] = data[normalised_field_name].str.replace(county + addRegex, '', case=False)
+
         return data
 
-    @classmethod
-    def parse(cls, data, normalised_field_name='ADDRESS_norm'):
+    def parse(self, data, normalised_field_name='ADDRESS_norm'):
         """
         Parse the address information given in the data.
 
@@ -99,9 +201,12 @@ class AddressParser:
         :return: parsed address data
         :rtype: pandas.DataFrame
         """
-        data = cls._normalize_input_data(data, normalised_field_name=normalised_field_name)
+        self.log.info('Start parsing address data...')
+
+        data = self._normalize_input_data(data, normalised_field_name=normalised_field_name)
 
         addresses = data[normalised_field_name].values
+        self.log.info('{} addresses to parse...'.format(len(addresses)))
 
         # temp data storage lists
         organisation = []
@@ -117,6 +222,34 @@ class AddressParser:
         # loop over addresses and use the probabilistic parser to tag the address components - should avoid a loop
         for address in tqdm(addresses):
             parsed = parser.tag(address.upper())
+            possible_postcode = self._extract_postcode(address)  # regular expression extraction
+
+            # if both parsers found postcode then check that they are the same
+            if parsed.get('Postcode', None) is not None and possible_postcode is not None:
+                if parsed['Postcode'] != possible_postcode:
+                    # not the same, use possible_postcode
+                    parsed['Postcode'] = possible_postcode
+
+            # if the probabilistic parser did not find postcode but regular expression did, then use that
+            if parsed.get('Postcode', None) is None and possible_postcode is not None:
+                parsed['Postcode'] = possible_postcode
+
+            if parsed.get('Postcode', None) is not None:
+                # check that there is space, if not then add if the parsed postcode is long enough to contain a complete
+                # postcode. Some users have partial postcodes to which one should not add a space.
+                if ' ' not in parsed['Postcode'] and len(parsed['Postcode']) > 4:
+                    in_code = parsed['Postcode'][-3:]
+                    out_code = parsed['Postcode'].replace(in_code, '')
+                    parsed['Postcode'] = out_code + ' ' + in_code
+
+                # change to all capitals
+                parsed['Postcode'] = parsed['Postcode'].upper()
+
+            # if Hackney etc. in StreetName then remove and move to locality if town name contains London
+            # Probabilistic parser should see more cases with london localities, parsed incorrectly at the mo
+            if parsed.get('StreetName', None) is not None and parsed.get('TownName', None) is not None:
+                if 'LONDON' in parsed['TownName']:
+                    parsed = self._fix_london_boroughs(parsed, os.path.join(self.currentDirectory, '../../data/'))
 
             # store the parsed information to separate lists
             organisation.append(parsed.get('OrganisationName', None))
@@ -142,7 +275,7 @@ class AddressParser:
         data['PAOText'] = data['BuildingName'].copy()
         data['SAOText'] = data['SubBuildingName'].copy()
 
-        data = cls._parser_postprocessing(data)
+        data = self._parser_postprocessing(data)
 
         return data
 
@@ -340,3 +473,32 @@ class AddressParser:
         data.loc[msk, 'SAOStartNumber'] = data.loc[msk, 'SubBuildingName']
 
         return data
+
+    @staticmethod
+    def convert_to_numeric_and_add_dummies(data):
+        """
+
+        :param data:
+        :return:
+        """
+        for numeric_columns in ('PAOstartNumber', 'PAOendNumber', 'SAOStartNumber', 'SAOEndNumber'):
+            data[numeric_columns] = pd.to_numeric(data[numeric_columns], errors='coerce')
+            data[numeric_columns].fillna(-12345, inplace=True)
+            data[numeric_columns] = data[numeric_columns].astype(np.int32)
+
+        for dummies_columns in ('PAOstartSuffix', 'PAOendSuffix', 'SAOStartSuffix', 'SAOEndSuffix'):
+            # if SubBuilding name or BuildingSuffix is empty add dummy - helps when comparing against None
+            msk = data[dummies_columns].isnull()
+            data.loc[msk, dummies_columns] = 'Not/Avail'
+
+        # for some welsh addresses the building name is parsed as organisation name, so place to PAOtext if empty
+        msk = data['PAOText'].isnull()
+        data.loc[msk, 'PAOText'] = data['OrganisationName']
+        msk = data['PAOText'].isnull()
+        data.loc[msk, 'PAOText'] = ''
+        msk = data['SAOText'].isnull()
+        data.loc[msk, 'SAOText'] = 'N/A'
+
+        # fill columns that are often NA with empty strings - helps when doing string comparisons against Nones
+        columns_to_add_empty_strings = ['OrganisationName', 'DepartmentName', 'SubBuildingName']
+        data[columns_to_add_empty_strings].fillna('', inplace=True)
