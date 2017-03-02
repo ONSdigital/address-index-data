eaee95706bea6645e1250654e07b4fc10f12c4c3
Sami Niemi <Sami.Niemi@valtech.co.uk>
2017-03-02 10:52:48 +0000

    Updated he address linker to use the address parser class.

--- DataScience/Analytics/linking/addressParser.py
+++ DataScience/Analytics/linking/addressParser.py
@@ -76,7 +76,9 @@ class AddressParser:
         :return: postcode
         :rtype: str
         """
-        regx = r'(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))'
+        regx = r'(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|' + \
+               '(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9]' + \
+               '[abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))'
         try:
             potential_postcode = re.findall(regx, string)[0][0]
             potential_postcode = potential_postcode.lower().strip()
