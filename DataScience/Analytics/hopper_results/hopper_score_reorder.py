d67427f0523d4adcbccbbfac592cf8ac35bca07a
ivyONS <26869198+ivyONS@users.noreply.github.com>
2017-09-18 21:28:28 +0100

    to be compared
    note that I removed candidates with bespoke scoring leq 0 (ideally this should remove wrong matches..)

--- DataScience/Analytics/hopper_results/hopper_score_reorder.py
+++ DataScience/Analytics/hopper_results/hopper_score_reorder.py
@@ -14,7 +14,7 @@ import json
 
 # Set up file path and file names
 tdata_path = '//tdata8/AddressIndex/Beta_Results/'
-datasets=['EdgeCases', 'LifeEvents',  'WelshGov2',  'WelshGov3', 'CQC', 'PatientRecords', 'WelshGov']
+datasets=['EdgeCases']#, 'LifeEvents',  'WelshGov2',  'WelshGov3', 'CQC', 'PatientRecords', 'WelshGov']
 
 def add_hopper_score(data):
     """
@@ -91,60 +91,90 @@ def check_performance_karen(data):
     :rtype: pandas.DataFrame
     """
 
-    data['hopper_results'] = ''
+    data['elastic_score'] = data['score']
+    data['elastic_results']=data['results']
+    data['score'] = data['structural_score']*1000 + data['object_score']
+
+    #in case of hopper score we need to filter only candidates with score >0
+    msk2 = data['score'] > 0
+    data['elastic_matches'] = data['matches']
+    data['matches'] = data['matches'] & msk2
+    data.loc[~msk2,'UPRN_beta'] = None
     
     # classifications that only need to look at one line at a time:
     msk0 = (data['UPRN_comparison'].notnull()) #| (data['UPRN_comparison'] =='')
     msk1 = (data['UPRN_beta'].isnull()) #| (data['UPRN_beta'] =='')
-    data.loc[msk1&msk0, 'hopper_results'] = '3_not_found'
-    data.loc[~msk1&~msk0, 'hopper_results'] = '5_new_uprn'
-    data.loc[msk1&~msk0, 'hopper_results'] = '6_both_missing'
-    data.loc[~msk1&msk0, 'hopper_results'] = '4_wrong_match'  # default value - correct matches will be overwritten
+    data.loc[msk1&msk0, 'results'] = '3_not_found'
+    data.loc[~msk1&~msk0, 'results'] = '5_new_uprn'
+    data.loc[msk1&~msk0, 'results'] = '6_both_missing'
+    data.loc[~msk1&msk0, 'results'] = '4_wrong_match'  # default value - correct matches will be overwritten
     
     # classifications where we need to look at all candidates at the same time:
     # add helper columns and then combine them to create appropriate filters
-    data['score_unique'] = data.groupby(['ID_original', 'hopper_score'])['hopper_score'].transform('count') == 1
-    data['score_min'] = data.groupby(['ID_original'])['hopper_score'].transform('min') == data['hopper_score']
-    data['top_match'] = data['matches'] & data['score_min']
+    data['score_unique'] = data.groupby(['ID_original', 'score'])['score'].transform('count') == 1
+    data['score_max'] = data.groupby(['ID_original'])['score'].transform('max') == data['score']
+    data['top_match'] = data['matches'] & data['score_max'] & data['score']
     data['top_match_group'] = data.groupby(['ID_original'])['top_match'].transform('sum')>0
     data['unique_top_match'] = data['top_match'] & data['score_unique']
     data['unique_top_match_group'] = data.groupby(['ID_original'])['unique_top_match'].transform('sum')>0
     data['match_group'] = data.groupby(['ID_original'])['matches'].transform('sum')>0
-    
-    data.loc[data['match_group'],'hopper_results'] = '2_in_set_lower'
-    data.loc[data['top_match_group'],'hopper_results'] = '2_in_set_equal'
-    data.loc[data['unique_top_match_group'],'hopper_results'] = '1_top_unique'
-    
-    data.drop(['prev_struc_score','prev_obj_score','score_unique','score_min','top_match','top_match_group', 'unique_top_match', 'unique_top_match_group', 'match_group'], axis=1, inplace=True)
 
+    data.loc[data['match_group'],'results'] = '2_in_set_lower'
+    data.loc[data['top_match_group'],'results'] = '2_in_set_equal'
+    data.loc[data['unique_top_match_group'],'results'] = '1_top_unique'
+
+    # Drop the duplicates so that there is only one entry for each address (if there is choice keep the one with existing UPRN).
+    deduped_original = data.copy()
+    deduped_original = deduped_original.sort_values(['ADDRESS', 'score', 'UPRN_beta', 'matches',  'UPRN_comparison'], 
+        ascending = [True, False, True, False, True], na_position='last').drop_duplicates(subset='ADDRESS', keep='first')
+        
+    results = deduped_original.results.value_counts().sort_index().to_dict()
+
+    result_names = [ '1_top_unique','2_in_set_equal', '2_in_set_lower', '3_not_found', '4_wrong_match','5_new_uprn', '6_both_missing']
+    all_results = pd.Series([results.get(key,0) for key in result_names])
+    all_results.index = result_names   
+    
+    print(all_results)
+    
+    #switch the names back
+    data['hopper_results'] = data['results']
+    data['hopper_score'] = data['score']
+    data['results'] = data['elastic_results']
+    data['score'] = data['elastic_score']
+    data['matches'] = data['elastic_matches']    
+    data.drop(['score_unique','score_max','top_match','top_match_group', 'unique_top_match', 'unique_top_match_group', 'match_group', 
+        'elastic_results', 'elastic_score', 'elastic_matches'], axis=1, inplace=True)
+     
     return data
 
 for dataset in datasets:
     
     # Iterate over all datasets
-    in_filename = tdata_path + dataset + '/August_24_dev_hopper_score/' + dataset + '_beta_DedupExist.csv'
-    out_filename = tdata_path + dataset + '/August_24_dev_hopper_score/' + dataset + '_hopper_score_results.csv'
-    out_filename_dedup = tdata_path + dataset + '/August_24_dev_hopper_score/' + dataset + '_hopper_score_results_no_duplicates.csv'
-    out_filename_pivot = tdata_path + dataset + '/August_24_dev_hopper_score/' + dataset + '_pivot.csv'
+    dir_date = '/September_15_dev_baseline/'#/August_24_dev_hopper_score/' 
+    in_filename = tdata_path + dataset + dir_date + dataset + '_beta_DedupExist.csv'
+    out_filename = tdata_path + dataset + dir_date + dataset + '_hopper_score_results.csv'
+    out_filename_dedup = tdata_path + dataset + dir_date + dataset + '_hopper_score_results_no_duplicates.csv'
+    out_filename_pivot = tdata_path + dataset + dir_date + dataset + '_pivot_ivy.csv'
     
     # Import data
     data = pd.read_csv(in_filename, encoding='latin-1')
     
     # Run functions
     data = add_hopper_score(data)
-    data = hopper_score_duplicates(data)
     data = check_performance_karen(data)
     
     # Export results
     data.to_csv(out_filename)
     
     # Without duplicates ie. without five possible matched addresses, just one for summary
-    data_dedup = data.drop_duplicates(subset='ID_original')
-    data_dedup.to_csv(out_filename_dedup)
+    data_dedup = data.sort_values(['ADDRESS', 'hopper_score', 'matches'], 
+        ascending = [True, False, False], na_position='last').drop_duplicates(subset='ADDRESS', keep='first')
+
+    #data_dedup.to_csv(out_filename_dedup)
     
     # Create pivot table comparing Elastic score and Hopper score results
     pivot = data_dedup.pivot_table(index = 'results', columns = 'hopper_results',
-                                   values = 'ADDRESS', aggfunc=lambda x: len(x.unique()))
+                                   values = 'ADDRESS', aggfunc=len)
     # Replace NaN with zeros
     pivot.fillna(0, inplace=True)
     pivot.to_csv(out_filename_pivot)
@@ -187,9 +217,9 @@ def add_debug_codes(data):
 
 # Add debug scores for Edge Cases
 dataset = 'EdgeCases'
-out_filename_dedup = tdata_path + dataset + '/July_31_dev_hopper_score/' + dataset + '_hopper_score_results_no_duplicates.csv'
+out_filename_dedup = tdata_path + dataset + '/August_24_dev_hopper_score/' + dataset + '_hopper_score_results_no_duplicates.csv'
 
 # Import data
-data = pd.read_csv(out_filename_dedup, encoding='latin-1')
-data = add_debug_codes(data)
-data.to_csv(out_filename_dedup)
+#data = pd.read_csv(out_filename_dedup, encoding='latin-1')
+#data = add_debug_codes(data)
+#data.to_csv(out_filename_dedup)
