96fe1dc7c2665d630e65a500ee1e0f14a7601ca8
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2018-01-23 15:06:50 +0000

    Mapping updated to use classic similarity.

--- batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
@@ -8,6 +8,13 @@ object Mappings {
      "settings": {
        "number_of_shards": 1,
        "number_of_replicas": 3,
+       "index": {
+         "similarity": {
+           "default": {
+             "type": "classic"
+           }
+         }
+       },
        "analysis": {
          "filter": {
            "address_synonym_filter": {
