0a9a00ae1790000a30b5701c8704cf1f5d7866ac
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2018-02-07 11:47:49 +0000

    Mapping shard update.

--- batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
@@ -6,8 +6,8 @@ object Mappings {
   """
    {
      "settings": {
-       "number_of_shards": 1,
-       "number_of_replicas": 3,
+       "number_of_shards": 2,
+       "number_of_replicas": 1,
        "index.queries.cache.enabled": false,
        "index": {
          "similarity": {
