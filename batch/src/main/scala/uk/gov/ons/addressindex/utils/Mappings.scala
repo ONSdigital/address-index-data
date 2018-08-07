c4994b2e225372b305ac25b7df0954fd3dad6be4
Paul-Joel <paul.joel@ons.gov.uk>
2018-08-07 15:27:35 +0100

    Revised mappings for partial endpoint

--- batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
@@ -259,22 +259,16 @@ object Mappings {
              "max_shingle_size": 2,
              "output_unigrams": false
            },
-            "autocomplete_filter": {
-              "type": "edge_ngram",
-              "min_gram": "4",
-              "max_gram": "10",
-              "token_chars": [
-                "letter"
-              ]
-            },
-            "autocomplete_number": {
-              "type": "edge_ngram",
-              "min_gram": "2",
-              "max_gram": "5",
-              "token_chars": [
-                "digit"
-              ]
-            }
+           "english_poss_stemmer": {
+              "type": "stemmer",
+              "name": "possessive_english"
+           },
+           "edge_ngram": {
+             "type": "edgeNGram",
+             "min_gram": "1",
+             "max_gram": "25",
+             "token_chars": ["letter", "digit"]
+           }
          },
          "analyzer": {
            "welsh_no_split_analyzer": {
@@ -296,22 +290,6 @@ object Mappings {
                "address_synonym_filter"
              ]
            },
-           "autocomplete": {
-            "filter": [
-              "lowercase",
-              "autocomplete_filter"
-             ],
-            "type": "custom",
-            "tokenizer": "standard"
-            },
-            "autocompleteNumber": {
-             "filter": [
-               "lowercase",
-               "autocomplete_number"
-              ],
-             "type": "custom",
-             "tokenizer": "standard"
-             },
            "welsh_bigram_analyzer": {
              "type": "custom",
              "tokenizer": "standard",
@@ -319,6 +297,14 @@ object Mappings {
                "asciifolding",
                "shingle_filter"
              ]
+           },
+           "edge_ngram_analyzer": {
+             "filter": ["lowercase", "english_poss_stemmer", "edge_ngram"],
+             "tokenizer": "standard"
+           },
+           "keyword_analyzer": {
+             "filter": ["lowercase", "english_poss_stemmer"],
+             "tokenizer": "standard"
            }
          },
          "tokenizer": {
@@ -505,14 +491,10 @@ object Mappings {
                      "type": "text",
                      "analyzer": "welsh_bigram_analyzer"
                    },
-                    "typeahead": {
-                      "type": "text",
-                      "analyzer": "autocomplete",
-                      "index_options": "docs"
-                    },
-                    "typeaheadNumber": {
-                      "type": "text",
-                      "analyzer": "autocompleteNumber"
+                    "partial": {
+                      "search_analyzer": "keyword_analyzer",
+                      "type": "string",
+                      "analyzer": "edge_ngram_analyzer"
                     }
                  }
                },
