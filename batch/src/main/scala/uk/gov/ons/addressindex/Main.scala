023044299564fd37a2a203fb07a93d18565013e3
Richard Smith <richard.m.smith@ons.gov.uk>
2019-07-19 11:30:47 +0100

    revert main

--- batch/src/main/scala/uk/gov/ons/addressindex/Main.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/Main.scala
@@ -37,25 +37,24 @@ For usage see below:
   val nodes = config.getString("addressindex.elasticsearch.nodes")
   val port = config.getString("addressindex.elasticsearch.port")
 
-  // each run of this application has a unique index name
-//  val indexName = generateIndexName(historical = !opts.hybridNoHist(), skinny = opts.skinny(), nisra = opts.nisra())
-//
-//  val url = s"http://$nodes:$port/$indexName"
-//
-//  if (!opts.help()) {
-//    AddressIndexFileReader.validateFileNames()
-//
-//    postMapping(indexName, skinny = opts.skinny())
-//    preLoad(indexName)
-//    saveHybridAddresses(historical = !opts.hybridNoHist(), skinny = opts.skinny(), nisra = opts.nisra())
-//    postLoad(indexName)
-//  } else opts.printHelp()
+ //  each run of this application has a unique index name
+  val indexName = generateIndexName(historical = !opts.hybridNoHist(), skinny = opts.skinny(), nisra = opts.nisra())
 
- // val indexName = generateIndexName(historical = true, skinny = true, nisra = true)
-  val indexName = "nisratest41_curent"
   val url = s"http://$nodes:$port/$indexName"
-  postMapping(indexName, skinny = true)
-  saveHybridAddresses(historical = true, skinny = true, nisra = false)
+
+  if (!opts.help()) {
+    AddressIndexFileReader.validateFileNames()
+
+    postMapping(indexName, skinny = opts.skinny())
+    preLoad(indexName)
+    saveHybridAddresses(historical = !opts.hybridNoHist(), skinny = opts.skinny(), nisra = opts.nisra())
+    postLoad(indexName)
+  } else opts.printHelp()
+
+//  val indexName = generateIndexName(historical = true, skinny = true, nisra = true)
+//  val url = s"http://$nodes:$port/$indexName"
+//  postMapping(indexName, skinny = true)
+//  saveHybridAddresses(historical = true, skinny = true, nisra = false)
 
   private def generateIndexName(historical: Boolean = true, skinny: Boolean = false, nisra: Boolean = false): String =
     AddressIndexFileReader.generateIndexNameFromFileName(historical, skinny, nisra)
