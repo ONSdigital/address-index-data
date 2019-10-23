53a467e8fc1aed1aaf683063bb55d9ec00a816da
Richard Smith <richard.m.smith@ons.gov.uk>
2019-10-23 17:47:07 +0100

    fat nisra code fixed including dependentlocality wrong way round

--- batch/src/main/scala/uk/gov/ons/addressindex/models/HybridAddressSkinnyNisraEsDocument.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/models/HybridAddressSkinnyNisraEsDocument.scala
@@ -160,18 +160,41 @@ object HybridAddressSkinnyNisraEsDocument extends EsDocument {
     val trimmedAltThoroughfare = normalize(altThoroughfare)
     val trimmedDependentThoroughfare = normalize(dependentThoroughfare)
 
-    val buildingNumberWithStreetDescription = s"${buildingNumber.toUpperCase} $trimmedDependentThoroughfare"
+    val buildingNumberWithStreetDescription = s"${buildingNumber.toUpperCase} $trimmedThoroughfare"
+    val buildingNameWithStreetDescription = s"${trimmedBuildingName.toUpperCase} $trimmedThoroughfare"
+    val commalessNumberAndStreetPart = if (startsWithNumber.findFirstIn(trimmedBuildingName).isDefined) buildingNameWithStreetDescription else buildingNumberWithStreetDescription
 
     Array(
-      Seq(normalize(organisationName), trimmedSubBuildingName, trimmedBuildingName, buildingNumberWithStreetDescription,
-        trimmedThoroughfare, normalizeTowns(locality), normalizeTowns(townland), normalizeTowns(townName),
+      Seq(normalize(organisationName),
+        trimmedSubBuildingName,
+        if (startsWithNumber.findFirstIn(trimmedBuildingName).isDefined) "" else trimmedBuildingName,
+        commalessNumberAndStreetPart,
+        trimmedDependentThoroughfare,
+        normalizeTowns(locality),
+        normalizeTowns(townland),
+        normalizeTowns(townName),
         postcode.toUpperCase).map(_.trim).filter(_.nonEmpty).mkString(", "),
       if (!altThoroughfare.isEmpty)
-        Seq(normalize(organisationName), trimmedSubBuildingName, trimmedBuildingName, buildingNumberWithStreetDescription,
-          trimmedAltThoroughfare, normalizeTowns(locality), normalizeTowns(townland), normalizeTowns(townName),
+        Seq(normalize(organisationName),
+          trimmedSubBuildingName,
+          trimmedBuildingName,
+          buildingNumber,
+          trimmedAltThoroughfare,
+          normalizeTowns(locality),
+          normalizeTowns(townland),
+          normalizeTowns(townName),
           postcode.toUpperCase).map(_.trim).filter(_.nonEmpty).mkString(", ") else "",
-      Seq(organisationName, subBuildingName, buildingName, buildingNumber, dependentThoroughfare, thoroughfare,
-        altThoroughfare, locality, townland, townName, postcode).map(_.trim).filter(_.nonEmpty).mkString(" ")
+      Seq(organisationName,
+        subBuildingName,
+        buildingName,
+        buildingNumber,
+        thoroughfare,
+        dependentThoroughfare,
+        altThoroughfare,
+        locality,
+        townland,
+        townName,
+        postcode).map(_.trim).filter(_.nonEmpty).mkString(" ")
     )
   }
 }
